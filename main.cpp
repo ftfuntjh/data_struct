cbb70ba5f11f9b8fd806c5067049a959c64d7d35
ftfunjth <imallrightgg@gmail.com>
2020-02-13 23:49:14 +0800

    add heap_sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "AVLTree.h"
 #include "RedBlackTree.h"
 #include "QuickSort.h"
+#include "HeapSort.h"
 
 int main(int argc, char *argv[]) {
     AVLTree avl_tree{1, 2, 3, 4, 5, -1, -2};
@@ -8,6 +9,8 @@ int main(int argc, char *argv[]) {
     rb_tree.remove(19);
     QuickSort qs{8, 2, 10, 7, 9};
     auto &p = qs.sort();
+    HeapSort hs{7, 6, 5, 4, 3, 2, 1};
+    auto &result = hs.sort();
     return 0;
 }
 
